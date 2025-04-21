9c1178290eaf9ebe65d6403cf7183842166c7cc8
Julian <48520440@est.ort.edu.ar>
2025-04-21 10:03:26 -0300

    terminado con extrasss

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-import express from 'express'; // Importa el módulo express
+import express from 'express'; 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // para poder leer JSON en POST y PUT
+app.use(express.json()); 
 
 let usuario = [{nombre: "Ana", edad: "25"}]
 let productos=["Mouse", "Teclado", "Monitor"]
 let materias=[{nombre: "Matematica"}, {nombre: "Lengua"}]
-
+let personas=[];
 
 app.get('/', (req, res) => {
-    res.json('Bienvenido a mi servidor!');
+    res.send('Bienvenido a mi servidor!');
   });
 
 
-  app.get('/saludo', (req, res) => {
-  res.json("Hola Mundo!!");
+app.get('/saludo', (req, res) => {
+  res.send("Hola Mundo!!");
 });
 
 app.get('/numero', (req, res) => {
-    res.json("42");
+    res.send("42");
   });
 
 app.get('/usuario', (req, res) => {
-    return res.status(400).json(usuario);
+  res.json(usuario)
 });
 
 app.get('/productos', (req, res) => {
-    return res.status(400).json(productos);
+   res.json(productos);
 });
 
 app.get('/materias', (req, res) => {
-    return res.status(400).json(materias);
+    res.status(400).json(materias);
 });
 
 
@@ -39,4 +39,20 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-// Para probar la API, puedes usar herramientas como Postman.
+app.post('/personas', (req, res) => {
+  const persona = req.body;
+  if (!persona) return res.status(400).json({ error: 'Faltan datos!' });
+  personas.push(persona);
+  res.send(personas)
+}  
+);
+
+app.get('/personas', (req, res) => {
+  res.json(personas)
+});
+
+app.delete('/personas/:id', (req,res)=>{
+  const id=parseInt(req.params.id);
+  personas.splice(id,1)
+  res.json(personas)
+})
